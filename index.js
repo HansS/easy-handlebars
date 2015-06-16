f27aad2c17fe4a4af97ba635649d648df275ba9d
Roger Jose Padilla Camacho <rogerjose81@gmail.com>
2015-06-16 18:17:16 -0500

    Added support for capturing the name from the path, by using a regexp.

--- index.js
+++ index.js
@@ -1,105 +1,96 @@
 'use strict';
 
 var fs = require('fs');
-var glob = require('glob');
+var globby = require('globby');
 var through = require('through2');
 var xtend = require('xtend');
 var Handlebars = require('handlebars');
 
-function arrToObj(arr) {
-    var resp = {};
-    if (arr) {
-        arr.forEach(function (entry) {
-            resp[entry] = true;
-        });
-    }
-    return resp;
-}
 
 function EasyHandlebars(files, opts) {
-    
-    if (!opts) {
-        opts = {};
-    }
-    
-    if (!opts.detectPartial) {
-        // Compile all as partials by default.
-        opts.detectPartial = opts.detectPartial || /./;   
-    }    
 
-    if (!(this instanceof EasyHandlebars)) {
-        return new EasyHandlebars(files, opts);
-    }
+  if (!(this instanceof EasyHandlebars)) {
+    return new EasyHandlebars(files, opts);
+  }
 
-    this.opts = this.normalizeOpts(opts);
+  this.opts = EasyHandlebars.normalizeOpts(opts);
 
-    return this.generateBundle(files);
+  return this.generateBundle(files);
 }
 
-EasyHandlebars.prototype.generateBundle = function (files) {
-    
-    var self = this;
+EasyHandlebars.prototype.generateBundle = function(files) {
 
-    var header = '(function (global, factory) {\n' +
-            'if (typeof module === "object" && typeof module.exports === "object") {\n' +
-            'module.exports  = factory;\n' +
-            '} else {\n' +
-            'factory(global.Handlebars);\n' +
-            '}' +
-            '}(typeof window !== "undefined" ? window : this, function (Handlebars) {\n';
+  var self = this;
 
-    var stream = through();
+  var stream = through();
 
-    stream.write(header);
+  stream.write(
+    '(function (global, factory) {\n' +
+      'if (typeof module === "object" && typeof module.exports === "object") {\n' +
+        'module.exports  = factory;\n' +
+      '} else {\n' +
+        'factory(global.Handlebars);\n' +
+      '}' +
+    '}(typeof window !== "undefined" ? window : this, function (Handlebars) {\n'
+  );
 
-    glob.sync(files).forEach(function (filePath) {
-        var html = fs.readFileSync(filePath, 'utf-8');
-        var tplId = self.extractTplId(filePath);
-        var hasData = self.opts.noData[tplId] === undefined;
-        var compilerOpts = JSON.parse(JSON.stringify(self.opts.defaultCompilerOpts));
-        compilerOpts.data = hasData;
-        stream.write('\n\n');
-        var tpl = Handlebars.precompile(html, compilerOpts);
-        if (self.opts.detectPartial && self.opts.detectPartial.test(tplId)) {
-            stream.write('Handlebars.registerPartial("' + tplId + '", Handlebars.template(' + tpl + '));');
-        } else {
-            stream.write('Handlebars.templates["' + tplId + '"] = Handlebars.template(' + tpl + ');');
-        }
-    });
-
-    stream.write('\n\n');
-    stream.write('}));');
-    stream.end();
-
-    return stream;
-};
-
-EasyHandlebars.prototype.extractTplId = function (filePath) {
-    var tplStartPosId = filePath.lastIndexOf('/') + 1;
-    var fileName = filePath.slice(tplStartPosId);
-    var tplId;
-    if (typeof this.opts['prefix'] !== 'undefined') {
-        tplId = this.opts.prefix + fileName;
+  globby.sync(files).forEach(function(filePath) {
+    var html = fs.readFileSync(filePath, 'utf-8');
+    var tplId = self.extractTplId(filePath);
+    var hasData = self.opts.noData[tplId] === undefined;
+    var compilerOpts = JSON.parse(JSON.stringify(self.opts.defaultCompilerOpts));
+    compilerOpts.data = hasData;
+    stream.write('\n');
+    var tpl = Handlebars.precompile(html, compilerOpts);
+    if (self.opts.detectPartialPattern === undefined || self.opts.detectPartialPattern.test(tplId)) {
+      stream.write('Handlebars.registerPartial("' + tplId + '", Handlebars.template(' + tpl + '));');
     } else {
-        tplId = fileName;
+      stream.write('Handlebars.templates["' + tplId + '"] = Handlebars.template(' + tpl + ');');
     }
-    var extPost = tplId.lastIndexOf('.');
-    return extPost >= 0 ? tplId.slice(0, extPost) : tplId;
+  });
+
+  stream.write('\n\n}));');
+  stream.end();
+
+  return stream;
 };
 
-EasyHandlebars.prototype.normalizeOpts = function (opts) {
-    
-    if (opts) {
-        if (Array.isArray(opts.noData)) {
-            opts.noData = arrToObj(opts.noData);
-        }
-        if (opts.defaultCompilerOpts && Array.isArray(opts.defaultCompilerOpts.knownHelpers)) {
-            opts.defaultCompilerOpts.knownHelpers = arrToObj(opts.defaultCompilerOpts.knownHelpers);
-        }
-    }
-
-    return xtend({noData: {}, defaultCompilerOpts: {}}, opts);
+EasyHandlebars.prototype.extractTplId = function(filePath) {
+  var match = this.opts.nameCapturingPattern.exec(filePath);
+  var tplId = match.slice(1).join('/');
+  return tplId;
 };
+
+EasyHandlebars.normalizeOpts = function(opts) {
+
+  if (opts === undefined) {
+    opts = {};
+  } else {
+    if (Array.isArray(opts.noData)) {
+      opts.noData = EasyHandlebars.arrToObj(opts.noData);
+    }
+    if (opts.defaultCompilerOpts && Array.isArray(opts.defaultCompilerOpts.knownHelpers)) {
+      opts.defaultCompilerOpts.knownHelpers = EasyHandlebars.arrToObj(opts.defaultCompilerOpts.knownHelpers);
+    }
+  }
+
+  if (opts.nameCapturingPattern === undefined) {
+    // Capture the file name without the extension by default.
+    opts.nameCapturingPattern = /.*\/([^\.]+)/;
+  }
+
+  return xtend({noData: {}, defaultCompilerOpts: {}}, opts);
+};
+
+EasyHandlebars.arrToObj = function(arr) {
+  var resp = {};
+  if (arr) {
+    arr.forEach(function(entry) {
+      resp[entry] = true;
+    });
+  }
+  return resp;
+}
 
 
 module.exports = EasyHandlebars;
